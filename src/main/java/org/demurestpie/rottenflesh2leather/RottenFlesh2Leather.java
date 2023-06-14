b0677b255169ccf97e20a6c56f4956bf14b5c2b5
DemurestPie <spongocoel_faerie@aleeas.com>
2023-06-14 00:20:20 -0400

    - Added comments - Added smoker recipe

--- src/main/java/org/demurestpie/rottenflesh2leather/RottenFlesh2Leather.java
+++ src/main/java/org/demurestpie/rottenflesh2leather/RottenFlesh2Leather.java
@@ -5,6 +5,7 @@ import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.CampfireRecipe;
 import org.bukkit.inventory.FurnaceRecipe;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.SmokingRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class RottenFlesh2Leather extends JavaPlugin {
@@ -15,10 +16,19 @@ public final class RottenFlesh2Leather extends JavaPlugin {
     }
 
     private void leatherRecipe() {
+        // Result from cooking rotten flesh
         ItemStack result = new ItemStack(Material.LEATHER);
+
+        // Add furnace recipe
         FurnaceRecipe furnace_leather_recipe = new FurnaceRecipe(new NamespacedKey(this, "furnace_RF2L"), result, Material.ROTTEN_FLESH, 1.0f, 200);
         getServer().addRecipe(furnace_leather_recipe);
+
+        // Add campfire recipe
         CampfireRecipe campfire_leather_recipe = new CampfireRecipe(new NamespacedKey(this, "campfire_RF2L"), result, Material.ROTTEN_FLESH, 1.0f, 600);
         getServer().addRecipe(campfire_leather_recipe);
+
+        // Add smoker recipe
+        SmokingRecipe smoker_leather_recipe = new SmokingRecipe(new NamespacedKey(this,"smoker_RF2L"), result, Material.ROTTEN_FLESH, 1.0f, 100);
+        getServer().addRecipe(smoker_leather_recipe);
     }
 }
